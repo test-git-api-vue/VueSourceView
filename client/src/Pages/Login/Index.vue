2b3876f6415c4382a15348869fba606c06f01968
Dmitriy Boyko <dmitriy.boyko@arsis.ru>
2020-06-03 10:28:16 +0300

    Добавлен UI логин формы с простой логикой валидации и возможностью сбросить введенные данные, показать\скрыть пароль

---
+++ client/src/Pages/Login/Index.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-app-bar app color="primary" dark>
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
+
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+          />
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+          <span class="mr-2">Latest Release</span>
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </v-app-bar>
+
+<!-- <v-flex xs12 sm6 offset-sm3> -->
+<!-- <v-layout align-center> -->
+<v-card
+    class="mx-auto"
+    max-width="500"
+  >
+    <v-card-title class="title font-weight-regular justify-space-between">
+      <span>Вход</span>
+    </v-card-title>
+
+    <v-window>
+      <v-window-item :value="1">
+        <v-card-text>
+          <v-text-field
+            label="Имя пользователя GitHub"
+            type="text"
+            @keyup="validate"
+            value=""
+            v-model="login"
+            requred
+          ></v-text-field>
+        </v-card-text>
+      </v-window-item>
+
+      <v-window-item :value="1">
+        <v-card-text>
+          <v-text-field
+          id="passField"
+            label="Персональный токен"
+            :type="this.passwordMasked ? 'password' : 'text'"
+            @keyup="validate"
+            v-model="password"
+            requred>
+          </v-text-field>
+          <!-- <v-spacer/> -->
+        </v-card-text>
+      </v-window-item>
+    </v-window>
+
+     <v-divider></v-divider>
+
+ <v-card-actions>
+     <v-btn
+        :disabled="this.login == '' && this.password == ''"
+        color="error"
+        depressed
+        @click="reset"
+      >Сбросить
+      </v-btn>
+
+      <v-btn
+      id="showTokenBtn"
+        :disabled="this.password == ''"
+        color="warning"
+        depressed
+        @click="showPassword"
+      >
+        {{showTokenBtnCaption}}
+       </v-btn>
+     <v-spacer/>
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        depressed
+        @click="validate"
+      >
+        Войти
+      </v-btn>
+    </v-card-actions>
+
+</v-card>
+<!-- </v-layout> -->
+<!-- </v-flex> -->
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Login extends Vue {
+valid = true;
+passwordMasked = true;
+login = '';
+password = '';
+
+get showTokenBtnCaption () {
+  return this.passwordMasked ? 'Показать токен' : 'Скрыть токен'
+}
+
+validate () {
+  this.valid = this.login !== '' && this.password !== ''
+}
+
+reset () {
+  this.login = ''
+  this.password = ''
+  this.valid = true
+}
+
+showPassword () {
+// todo менять текст
+  this.passwordMasked = !this.passwordMasked
+}
+}
+</script>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,22 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../Pages/Login/Index.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Login',
+    component: Login
   }
 ]
 
@@ -24,6 +16,8 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+(Vue as any).router = router
 
 export default router
